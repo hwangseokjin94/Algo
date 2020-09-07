d1dcd51a396f3fb4145dfb12a724e8c94e35c3f9
a <a@DESKTOP-N6LR41B>
2020-09-07 16:55:01 +0900

    문자열다루기 최종커밋

--- Algo/src/programers/문자열다루기기본.java
+++ Algo/src/programers/문자열다루기기본.java
@@ -6,8 +6,7 @@ public class 문자열다루기기본 {
 		char arr[] = s.toCharArray();
 		char num[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
 		int count = 0;
-		if(s.length() != 4 && s.length()!=6){
-			
+		if(s.length() == 4 || s.length()==6){			
 			for (int i = 0; i < arr.length; i++) {
 				for (int j = 0; j < num.length; j++) {
 					if (num[j] == arr[i]) {
@@ -17,16 +16,18 @@ public class 문자열다루기기본 {
 			}
 		}
 		
-		if (count == arr.length) {			
+		if (count == arr.length) {
+			System.out.println(answer);
 			return answer;
 		} else {			
 			answer = false;			
+			System.out.println(answer);
 			return answer;
 		}
 	}//
 
 	public static void main(String[] args) {
-		String s = "2 3459";
+		String s = "1234";
 		solution(s);
 	}
 
