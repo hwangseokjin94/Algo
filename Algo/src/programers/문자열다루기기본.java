2b5e07a9f5d89cf3a7fa36a6c4cf6381cdc81897
a <a@DESKTOP-N6LR41B>
2020-09-07 16:48:19 +0900

    문자열다루기기본 커밋

--- Algo/src/programers/문자열다루기기본.java
+++ Algo/src/programers/문자열다루기기본.java
@@ -6,20 +6,21 @@ public class 문자열다루기기본 {
 		char arr[] = s.toCharArray();
 		char num[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
 		int count = 0;
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < num.length; j++) {
-				if (num[j] == arr[i]) {
-					count++;
+		if(s.length() != 4 && s.length()!=6){
+			
+			for (int i = 0; i < arr.length; i++) {
+				for (int j = 0; j < num.length; j++) {
+					if (num[j] == arr[i]) {
+						count++;
+					}
 				}
 			}
 		}
-		if (count == arr.length) {
-			
+		
+		if (count == arr.length) {			
 			return answer;
-		} else {
-			
-			answer = false;
-			
+		} else {			
+			answer = false;			
 			return answer;
 		}
 	}//
